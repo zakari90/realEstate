ae9e0f395443f9da423b17168cf6a0dbcff64917
zakari90 <zakariazinedine1@gmail.com>
2024-11-30 13:54:12 +0100

    commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,50 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import createMiddleware from 'next-intl/middleware';
 import { NextRequest, NextResponse } from 'next/server';
-import { trackPageVisit, trackVisit } from './_actions/admin/admin';
 import { analytics } from './lib/analytics';
+import { trackVisit } from './_actions/admin/admin';
 
 const intlMiddleware = createMiddleware({
-  locales: ['en', 'fr', "ar"],
-  localePrefix: 'always',
-  defaultLocale: 'en',
+  locales: ['en', 'fr', 'ar'],
+  localePrefix: 'always', // Ensure locale prefix in the URL is always required
+  defaultLocale: 'en', // Default locale fallback
 });
 
 const isPublicRoute = createRouteMatcher(['/', '/(fr|en|ar)/:path*']);
 
+// Middleware function
 export default clerkMiddleware(async (auth, req) => {
-  // Call analytics middleware asynchronously
-  analyticsMiddleware(req);
-  
+  // First, execute the next-intl middleware to determine the locale
+  const localeResponse = await intlMiddleware(req);
+  if (localeResponse) {
+    // Return if locale-related issues are found
+    return localeResponse;
+  }
+
+  // Proceed with Clerk authentication middleware
   if (!isPublicRoute(req)) {
     auth().protect();
   }
 
-  return intlMiddleware(req); // Proceed with internationalization middleware
+  // Proceed with other operations like analytics tracking
+  analyticsMiddleware(req);
+
+  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'], // Match all routes except static assets
 };
 
+// Analytics Middleware
 async function analyticsMiddleware(req: NextRequest) {
   const supportedLocales = ['fr', 'en', 'ar'];
-  console.log("analyticsMiddleware");
 
   if (!req.nextUrl.pathname.startsWith('/_next') && !req.nextUrl.pathname.includes('api')) {
     try {
-      // Track analytics asynchronously in the background
       if (supportedLocales.some(locale => req.nextUrl.pathname.startsWith(`/${locale}/`))) {
         console.log('Matched path for locale:', req.nextUrl.pathname);
-        
-        analytics.track('pageview', {
+        await analytics.track('pageview', {
           page: req.nextUrl.pathname,
           country: req.geo?.country,
         }).catch(error => {
@@ -45,7 +52,7 @@ async function analyticsMiddleware(req: NextRequest) {
         });
       }
     } catch (error) {
-      console.error("analyticsMiddleware", error);
+      console.error("Error in analytics middleware", error);
     }
   }
 
