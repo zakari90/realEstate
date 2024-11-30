764702118646bea69ef2062bac8dff7608e87a54
zakari90 <zakariazinedine1@gmail.com>
2024-11-30 23:46:45 +0100

    edge function

--- src/_actions/admin/admin.ts
+++ src/_actions/admin/admin.ts
@@ -25,31 +25,31 @@ export async function trackPageVisit(pathname: string) {
 
 
 
-// export async function trackVisit(pagePath: string, location: string) {
-//     const visit = await db.visits.upsert({
-//       where: { pagePath },
-//       update: { totalVisits: { increment: 1 } },  // Increment visit count
-//       create: {
-//         pagePath,
-//         totalVisits: 1,
-//       },
-//     });
+export async function trackVisit(pagePath: string, location: string) {
+    const visit = await db.visits.upsert({
+      where: { pagePath },
+      update: { totalVisits: { increment: 1 } },  // Increment visit count
+      create: {
+        pagePath,
+        totalVisits: 1,
+      },
+    });
   
-//     // Log individual visit in WebVisitLog table
-//     await db.webVisitLog.create({
-//       data: {
-//         location,  // You can extract actual location from headers or geo info
-//         visitsId: visit.id,  // Link to the corresponding Visits table entry
-//       },
-//     });
-//   }
+    // Log individual visit in WebVisitLog table
+    await db.webVisitLog.create({
+      data: {
+        location,  // You can extract actual location from headers or geo info
+        visitsId: visit.id,  // Link to the corresponding Visits table entry
+      },
+    });
+  }
 
-// export async function trackPageVisit(page: string, ip: string) {
-//   return await db.analytics.create({
-//     data: {
-//       page,
-//       ip,
-//       createdAt: new Date()
-//     }
-//   })
-// }
+export async function trackPageVisit2(page: string, ip: string) {
+  return await db.analytics.create({
+    data: {
+      page,
+      ip,
+      createdAt: new Date()
+    }
+  })
+}
