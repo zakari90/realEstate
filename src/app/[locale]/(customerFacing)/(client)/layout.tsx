3b2c679c13d449b9669f76353a7cdd80ae21b691
zakari90 <zakariazinedine1@gmail.com>
2026-02-07 19:48:42 +0100

    correction

--- src/app/[locale]/(customerFacing)/(client)/layout.tsx
+++ src/app/[locale]/(customerFacing)/(client)/layout.tsx
@@ -1,7 +1,6 @@
 import ClientFooter from "@/components/clientFootre";
-import { NavBar, NavItem } from "@/components/clientNavbar";
+import { NavBar } from "@/components/clientNavbar";
 import type { Metadata } from "next";
-import { useTranslations } from "next-intl";
 import { Inter as FontSans } from "next/font/google";
 
 const fontSans = FontSans({
@@ -20,18 +19,18 @@ interface RootLayoutProps {
   };
 }
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
-  const t = useTranslations("navigation");
+  // const t = useTranslations("navigation");
 
-  const clientNavItems: NavItem[] = [
-    { href: "/", name: t("home") },
-    { href: "/properties", name: t("properties") },
-    { href: "/investments", name: t("investments") },
-    { href: "/aboutUs", name: t("aboutUs") },
-  ];
+  // const clientNavItems: NavItem[] = [
+  //   { href: "/", name: t("home") },
+  //   { href: "/properties", name: t("properties") },
+  //   { href: "/investments", name: t("investments") },
+  //   { href: "/aboutUs", name: t("aboutUs") },
+  // ];
   return (
     <>
       {/* <Header locale={locale} navLinks= {navLinks}  />         */}
-      <NavBar navItems={clientNavItems} />
+      <NavBar />
       <div className="flex-grow">{children}</div>
       <ClientFooter />
       {/* <Footer locale={locale} navLinks= {navLinks} /> */}
