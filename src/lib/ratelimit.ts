86b9c84dc5fbbb20a62300a9dcf2daf24f56c99b
zakari90 <zakariazinedine1@gmail.com>
2026-02-07 09:10:52 +0100

    correction

---
+++ src/lib/ratelimit.ts
@@ -0,0 +1,15 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+export const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis.
+   *
+   * @default "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
